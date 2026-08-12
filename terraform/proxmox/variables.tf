82b1488dce10a093ad1186cfcc72c9809b495bcb
ViniciusBrobowski <125225193+ViniciusBrobowski@users.noreply.github.com>
2026-08-11 22:28:43 -0300

    Configure Terraform Proxmox provider baseline (#32)
    * Configure Terraform Proxmox provider baseline
    
    Issue: #22
    
    * Polish Terraform provider documentation
    
    * Fix Terraform README code block

---
+++ terraform/proxmox/variables.tf
@@ -0,0 +1,16 @@
+variable "proxmox_endpoint" {
+  description = "Proxmox VE API endpoint URL"
+  type        = string
+}
+
+variable "proxmox_api_token" {
+  description = "Proxmox VE API token used by Terraform"
+  type        = string
+  sensitive   = true
+}
+
+variable "proxmox_insecure" {
+  description = "Disable TLS certificate verification for lab environments"
+  type        = bool
+  default     = false
+}
