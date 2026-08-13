f9897c3f9049276fc6692d75f231fd4e02b7626f
ViniciusBrobowski <125225193+ViniciusBrobowski@users.noreply.github.com>
2026-08-12 22:14:33 -0300

    Terraform variables were defined for VM provisioning in the Proxmox environment, and a safe terraform.tfvars.example file was added with placeholder values. (#37)
    Issue: #23

--- terraform/proxmox/variables.tf
+++ terraform/proxmox/variables.tf
@@ -14,3 +14,42 @@ variable "proxmox_insecure" {
   type        = bool
   default     = false
 }
+
+variable "vm_name" {
+  description = "Name of the virtual machine"
+  type        = string
+}
+
+variable "vm_cpu_cores" {
+  description = "Number of CPU cores for the virtual machine"
+  type        = number
+  default     = 2
+}
+
+variable "vm_memory_mb" {
+  description = "Memory assigned to the virtual machine in MB"
+  type        = number
+  default     = 2048
+}
+
+variable "vm_disk_size_gb" {
+  description = "Disk size for the virtual machine in GB"
+  type        = number
+  default     = 20
+}
+
+variable "proxmox_node" {
+  description = "Proxmox node where the VM will be created"
+  type        = string
+}
+
+variable "proxmox_storage" {
+  description = "Proxmox storage used for the virtual machine disk"
+  type        = string
+}
+
+variable "proxmox_network_bridge" {
+  description = "Proxmox network bridge for the virtual machine"
+  type        = string
+  default     = "vmbr0"
+}
